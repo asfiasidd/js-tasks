ee6260f0a31a3cecb45c43d20cb80ed4d4b053d9
asfiasidd <alihamzahameed123@gmail.com>
2023-04-16 10:11:10 -0700

    all tasks done

---
+++ scripts/assignment.js
@@ -0,0 +1,93 @@
+//   Tasks 1
+
+//   var zakat_percentage = 0.025; 
+//   var user_input =+ prompt("Calculate your Zakat");
+//   var Result = (zakat_percentage) * (user_input);
+//   var Result = ('Your zakat value is' + Result);
+//   alert(Result);
+
+//   Tasks 3
+
+// var secretnumber = 2;
+// var user_input = + prompt( "Here you can guess the number");
+// if (user_input == secretnumber) {alert ( " Great you can guess the number ");
+// }else if (user_input < secretnumber) { alert( " worng please try again");
+// } else if (user_input > secretnumber) { alert ( "worng please try again ");
+// }
+
+// Tasks 4
+
+// var user_name = prompt( " please enter your name");
+// user_name("Ali")
+// console.log (username.charAt(0).toUpperCase(0)+username.slice(1));
+//   alert (username);
+
+// Tasks 5
+
+// var contactname = []
+
+// var contactnumber = []
+
+// var cno = prompt('Number')
+
+// var Cname = prompt('Name')
+
+// contactname.push('name')
+
+// contactnumber.push('number')
+
+// console.log(contactname)
+
+// console.log(contactnumber)
+
+
+// Tasks 6
+
+
+// var items =["chicken biryani", "Golgappe","Pizza","kulfii","Gulab jumum"];
+// var user_input = + prompt( "Tell me what you want to eat");
+// var user_input2 = prompt("Select your items:  \n 1)Golgappe \n 2)Pizza \n 3)chicken biryani \n 4)kulfii \n 5)Gulab jumum");
+// var a= items.slice( user_input);
+// alert("food: hungry");
+// Result 
+// remainingitems=(a)
+// deleteitems [a]
+// alert("remaining items"+ "remaining product");
+
+// Tasks 7
+
+// var nationality = +prompt("Enter your nationality");
+// if (nationality == "pakistanii","indian") {alert("You are eligible to vote")
+// }
+// var gender = +prompt("Enter your gender");
+// if (gender == "Female","male") {alert("You are eligible to vote")
+// }
+// var age = +prompt("Enter your age");
+// if (age >=18) {alert(" You are eligible to vote")
+// }
+// else{ age <=18} {alert (" You are not eligible to vote")}
+
+
+// Tasks 8
+
+
+// var cricketplayer=[ "Babar Azam","haris ali","shadab khan","shaheen shah afridi","wasim jnr","Mohammad Rizwan","Naseem Shah","Haris Rauf","Fakhar Zaman","Mohammad Nawaz","Iftikhar Ahmed","Asif Ali","zaid ali","Imam-ul-haq","Khusdlii shah"]
+// console.log(cricketplayer);
+// var t20player = cricketplayer.slice (0,12);
+// alert(cricketplayer);
+
+// Tasks 2
+
+// ^Calculator the Fitra^
+
+var kismis = "1200"
+var Wheat = "180"
+var Almond = "300"
+var Walnuts ="407"
+var user_input1 =prompt("Total number of your family members:");
+var user_input2 = prompt("Select your items:    \n 1)kismis  RS=1200  \n 2)wheat Rs=180 \n 3)Almond  Rs= 300  \n 4)walnuts Rs= 407");
+if (user_input2 == "kismis"){ document.write("Your Fitra amount is:" +user_input1*kismis + "PKR")}
+if (user_input2 == "Wheat"){ document.write("Your Fitra amount is:" +user_input1*Wheat + "PKR")}
+if (user_input2 == "Almond"){ document.write("Your Fitra amount is:" +user_input1*Almond +"PKR")}
+if (user_input2 == "Walnuts"){ document.write("Your Fitra amount is:" +user_input1*Walnuts +"PKR")}
+alert( Total); 
